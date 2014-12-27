22b0331bbf2cc3759568dd7930defa447ff2d18a
Gareth Paul Jones <gjones@twitter.com>
2014-12-26 17:46:04 -0800

    inital

--- RecipeSwipe/RecipePickerView.swift
+++ RecipeSwipe/RecipePickerView.swift
@@ -1,10 +1,4 @@
 //
-//  LawyerPickerView.swift
-//  Linder
-//
-//  Created by Jeffrey Ching on 10/19/14.
-//  Copyright (c) 2014 Jeffrey Ching. All rights reserved.
-//
 
 import UIKit
 
--- RecipeSwipe/RecipeViewController.swift
+++ RecipeSwipe/RecipeViewController.swift
@@ -1,11 +1,4 @@
 //
-//  LawyerViewController.swift
-//  Linder
-//
-//  Created by Jeffrey Ching on 10/17/14.
-//  Copyright (c) 2014 Jeffrey Ching. All rights reserved.
-//
-
 import UIKit
 
 class RecipeViewController: UIViewController {
